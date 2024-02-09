420fd3f5ad6c6fdc9a4105b8cdc845dcd179facb
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2024-02-09 01:21:21 -0600

    Feature: Avance con la UI de los tasks

--- Assets/LVLManager.cs
+++ Assets/LVLManager.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LVLManager : MonoBehaviour
 {
@@ -10,19 +14,50 @@ public class LVLManager : MonoBehaviour
     Light luz;
     // AudioSource linterna;
 
+    // UI
+    private bool pause;
+    [SerializeField]
+    int SpecialItemsFound = 0;
+
+    public Text ItemsFoundDisplay;
+    
+    // tareas
+    private bool tareasOn;
+    [SerializeField]
+    GameObject listUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ItemsFoundDisplay = GetComponent<TextMesh>();
+        pause = false;
+        listUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // función de la linterna
         if (Input.GetMouseButtonDown(0))
         {
             luz.enabled = luz.enabled ? false : true;
             //linterna.Play();
         }
+
+        // Display del texto 
+        ItemsFoundDisplay.text = SpecialItemsFound + "/6";
+
+        // tareas 
+        if (Input.GetKeyDown(KeyCode.T)) {
+            Debug.Log("ttt  ");
+            tareasOn = !tareasOn; // toggle 
+
+            if (tareasOn) {
+                listUI.SetActive(false);
+            } else {
+                listUI.SetActive(true);
+            }
+        }
+
     }
 }
